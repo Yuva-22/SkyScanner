bf2cda3e4883d62b2b79a8c754a0bea159f44149
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-13 16:29:27 +0530

    Navigation bar was added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component{
     render(){
@@ -7,10 +8,15 @@ class Navbar extends Component{
         <nav className="NavbarItems">
            <h1 className="navbar-logo">SkyScanner</h1>
            <ul className="nav-menu">
-            <li>
-                <Link>
-                <i class="fa-solid fa-house-chimney"></i>Home</Link>
-            </li>
+              {MenuItems.map((item, index) =>{
+                return(
+                    <li key={index}>
+                       <a className={item.cname} href={item.url}>
+                        <i className={item.icon}></i>{item.title}
+                       </a>
+                    </li>
+                );
+              })}
            </ul>
         </nav>
        );
