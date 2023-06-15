9aab60580de06cd45fff15a7e798c6cc738dc626
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-15 16:12:06 +0530

    Contactpage was created

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,8 +1,18 @@
+import NavBar from "../components/Navbar";
+import Hero from "../components/Hero";
+import img7 from "../assets/img7.jpeg";
+
+
 function Contact(){
     return(
       <>
-      
-      <h1>Contact page</h1>
+      <NavBar />
+      <Hero 
+      cName="hero-mid"
+      heroImg={img7}
+      title="Contact Us"
+      btnclass="hide"
+      />
       </>
 
     )
