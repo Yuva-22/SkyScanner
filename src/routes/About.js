7e6f65a6f8214855dd836b4b8564bd1196e06917
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-15 13:26:53 +0530

    Homepage made responsive

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,21 @@
+import NavBar from "../components/Navbar";
+import Hero from "../components/Hero";
+import img2 from "../assets/img2.jpeg";
+
+
 function About(){
     return(
       <>
-      <h1>About page</h1>
+      <NavBar />
+      <Hero 
+      cName="hero-mid"
+      heroImg={img2}
+      title="About"
+      text="Choose Your Favourite Destination"
+      btntext="Travel plan"
+      url="/"
+      btnclass="show"
+      />
       
       </>
 
