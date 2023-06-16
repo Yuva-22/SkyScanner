adbcd41912880a93b8616685337106eaff1b7991
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-16 15:18:47 +0530

    Homepage modified

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,9 +1,14 @@
 import DestinationData from "./DestinationData";
 import "./Destinationstyles.css";
-import i1 from "../assets/1.jpg";
-import i2 from "../assets/2.jpg";
-import i3 from "../assets/3.jpg";
-import i4 from "../assets/4.jpg";
+import i1 from "../assets/Manali2.jpg";
+import i2 from "../assets/manali1.jpg";
+import i3 from "../assets/coorg1.jpg";
+import i4 from "../assets/coorg2.jpeg";
+import i5 from "../assets/mussoorie1.jpg";
+import i6 from "../assets/mussoorie2.jpg";
+import i7 from "../assets/darjeeling1.jpg";
+import i8 from "../assets/darjeeling2.jpg";
+
 
 
 const Destination = () =>{
@@ -16,42 +21,68 @@ const Destination = () =>{
 
         <DestinationData
         className="first-des"
-        heading="Taal volcano"
-        text="Eros in cursus turpis massa tincidunt 
-        dui. Condimentum id venenatis a condimentum. 
-        Urna neque viverra justo nec. Consectetur 
-        adipiscing elit duis tristique sollicitudin 
-        nibh. Fermentum odio eu feugiat pretium. 
-        A diam maecenas sed enim ut sem viverra 
-        aliquet. Velit ut tortor pretium viverra 
-        suspendisse potenti nullam. Volutpat commodo 
-        sed egestas egestas fringilla phasellus 
-        faucibus scelerisque. Dignissim diam quis 
-        enim lobortis scelerisque fermentum dui. 
-        Gravida neque convallis a cras semper. 
-        In fermentum posuere urna nec tincidunt 
-        praesent semper feugiat."
+        heading="MANALI-The Paradise"
+        text="With spectacular valleys, breathtaking views, 
+        snowcapped mountains, and lush forests of oak, 
+        deodar, and pine, Manali is a magical hill station 
+        at the northern end of Kullu valley in Himachal 
+        Pradesh. Gifted to the world by the mighty Himalayas, 
+        it is known to be one of the most popular destinations 
+        for Honeymooners. Manali offers magnificent views of 
+        the Pir Panjal and the Dhauladhar ranges, covered 
+        in a thick blanket of snow for most of the year."
         img1={i1}
         img2={i2}
         />
 
        <DestinationData
         className="first-des-reverse"
-        heading="Taal"
-        text=" Consectetur 
-        adipiscing elit duis tristique sollicitudin 
-        nibh. Fermentum odio eu feugiat pretium. 
-        A diam maecenas sed enim ut sem viverra 
-        aliquet. Velit ut tortor pretium viverra 
-        suspendisse potenti nullam. Volutpat commodo 
-        sed egestas egestas fringilla phasellus 
-        faucibus scelerisque. Dignissim diam quis 
-        enim lobortis scelerisque fermentum dui. 
-        Gravida neque convallis a cras semper. 
-        In fermentum posuere urna nec tincidunt 
-        praesent semper feugiat."
+        heading="COORG-The Scotland of India"
+        text="Coorg, officially known as Kodagu, is the most 
+        affluent hill station in Karnataka. It is well known 
+        for its breathtakingly exotic scenery and lush greenery. 
+        Forest covered hills, spice and coffee plantations 
+        only add to the landscape. Madikeri is the region's 
+        centre point with all transportation for getting around 
+        starting from here. On a visit to Coorg, cover the beautiful 
+        towns like Virajpet, Kushalnagar, Gonikoppal, Pollibetta, 
+        and Somwarpet, and experience the beautiful concept of 
+        homestays to make your experience more memorable! "
         img1={i3}
         img2={i4}
+        />
+
+        <DestinationData
+        className="first-des"
+        heading="MUSSOORIE-The Queen of HillStations"
+        text="Mussoorie is one of the most popular hill stations 
+        located at a distance of 290 km from Delhi in the 
+        Dehradun district of Uttarakhand. With a backdrop of the 
+        Shivalik range of Himalayas and doon valley, Mussoorie, 
+        also known as Queen of The Hills, stands at an altitude of 
+        7000 feet above sea level. With a cool and pleasant 
+        climate throughout the year,Mussoorie was once the 
+        British summer capital. One of the most popular tourist 
+        attractions in Mussoorie is The Mall, which is a slightly 
+        steep boulevard with eateries and other shops lining 
+        its entire stretch."
+        img1={i5}
+        img2={i6}
+        />
+
+        <DestinationData
+        className="first-des-reverse"
+        heading="DARJEELING-Queen of Himalayas"
+        text="Darjeeling, the former summer capital of India under 
+        the British Raj, has evolved into one of India's most 
+        sought-after hill stations. This picturesque hill destination 
+        in West Bengal is ideal for a romantic honeymoon. Darjeeling, 
+        nestled among acres of tea estates, is 2,050 metres above sea 
+        level and thus has a cool climate all year.The Darjeeling Himalayan
+        Railway established back in 1881 is conferred the title of 
+        World Heritage Status by UNESCO. "
+        img1={i7}
+        img2={i8}
         />
 
 
--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,8 +1,9 @@
 import "./TripStyles.css";
 import TripData from "./TripData";
-import i5 from "../assets/5.jpg";
-import i6 from "../assets/6.jpg";
-import i8 from "../assets/9.jpg";
+import i5 from "../assets/paris.jpg";
+import i6 from "../assets/img8.jpeg";
+import i7 from "../assets/maldives.jpg";
+import i8 from "../assets/allepey.jpg";
 
 function Trip(){
     return(
@@ -14,33 +15,52 @@ function Trip(){
 
             <TripData 
             image={i5}
-            heading="First Place"
-            text="Velit ut tortor pretium viverra 
-            suspendisse potenti nullam. Volutpat commodo 
-            sed egestas egestas fringilla phasellus 
-            faucibus scelerisque. Dignissim diam quis 
-            enim lobortis scelerisque fermentum dui. 
-            Gravida neque convallis a cras semper."            
+            heading="Paris-City of Lights"
+            text="Paris is the country's cosmopolitan capital.
+            The City of Love, as people call it, is said to be 
+            one of the most beautiful and romantic cities brimming 
+            with culture, art, fashion, food, and history. Home to 
+            some of the most popular iconic monuments and museums, 
+            Paris consists of a large part along the Seine designated 
+            as a UNESCO World Heritage Site.One of the most significant 
+            art repositories in the world, Paris harbors artistic lineage 
+            under Renoir, Rodin, Picasso, Monet, and Van Gogh."  
             />
             <TripData 
             image={i6}
-            heading="Second Place"
-            text="Velit ut tortor pretium viverra 
-            suspendisse potenti nullam. Volutpat commodo 
-            sed egestas egestas fringilla phasellus 
-            faucibus scelerisque. Dignissim diam quis 
-            enim lobortis scelerisque fermentum dui. 
-            Gravida neque convallis a cras semper."            
+            heading="Colosseum,Rome"
+            text="The Colosseum is one of the most famous attractions 
+            in Rome and is where most gladiatorial fights took place. 
+            Today, actors in gladiator clothes stand outside the 
+            Colosseum, where you can take pictures with them. 
+            The Colosseum is a testament to the ingenuity of the 
+            builders, having stood the test of time. While parts 
+            have been damaged, the Colosseum still remains standing 
+            in most of its original glory today."            
             />
             <TripData 
+            image={i7}
+            heading="Maldives-A Tropical Heaven"
+            text="The Maldives, a tropical haven of immaculate beaches, 
+            are an archipelago of 1,192 coral islands grouped into natural 
+            atolls, out of which only a few are inhabited. Time stands 
+            still in the Maldives as you lie on the white sand beaches, 
+            hearing the crystal clear waves crash against the seashore, 
+            swim with the manta rays and reef sharks and feast on a wide 
+            array of fishes with the locals. Be it adventure or leisure, 
+            Maldives is your answer."            
+            />
+
+           <TripData 
             image={i8}
-            heading="Third Place"
-            text="Velit ut tortor pretium viverra 
-            suspendisse potenti nullam. Volutpat commodo 
-            sed egestas egestas fringilla phasellus 
-            faucibus scelerisque. Dignissim diam quis 
-            enim lobortis scelerisque fermentum dui. 
-            Gravida neque convallis a cras semper."            
+            heading="Alleppey-Venice of East"
+            text="Officially called Alappuzha, Alleppey is known for 
+            its network of canals and lagoons with beautiful backwaters 
+            and the houseboats offering overnight stays. Located in the 
+            southern part of Kerala, the coastline of Alleppey offers 
+            some of the best beaches with water sports during the dry season.
+            Kuttanad, also called the 'Rice Bowl of Kerala' covers a large part
+            of Allapuzha and is home to lush green paddy fields, lakes and backwaters.  "            
             />
 
             </div>
