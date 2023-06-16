4d2b8dbd293bf88d7601f37b062a0785b9056b0f
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-16 12:55:16 +0530

    Contact form is added

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,6 +2,7 @@ import NavBar from "../components/Navbar";
 import Hero from "../components/Hero";
 import img7 from "../assets/img7.jpeg";
 import Footer from "../components/Footer";
+import ContactForm from "../components/ContactForm";
 
 
 function Contact(){
@@ -14,6 +15,7 @@ function Contact(){
       title="Contact Us"
       btnclass="hide"
       />
+      <ContactForm />
       <Footer />
       </>
 
