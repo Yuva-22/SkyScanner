48527e0c4b8dff83db7b5bcc95e6a7bc715c22dd
Yuvashree <yuvashree.m22122003@gmail.com>
2023-06-16 15:40:13 +0530

    About page modified

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,38 +3,44 @@ import "./AboutUsStyles.css";
 function AboutUs(){
     return(
        <div className="about-container">
-            <h1>Our History</h1>
-            <p>Montes nascetur ridiculus mus mauris vitae ultricies leo integer
-            Ut placerat orci nulla pellentesque dignissim enim sit. 
-            Sollicitudin nibh sit amet commodo nulla facilisi nullam. Turpis 
-            massa tincidunt dui ut. Vulputate sapien nec sagittis aliquam malesuada. 
-            Felis eget nunc lobortis mattis aliquam faucibus purus. Nibh nisl 
-            condimentum id venenatis a. Id volutpat lacus laoreet non curabitur 
-            gravida. Dolor sed viverra ipsum nunc. Bibendum ut tristique et 
-            egestas quis ipsum. Commodo quis imperdiet massa tincidunt nunc 
-            pulvinar sapien. </p>
+            <h1>SKYSCANNER</h1>
+            <p>At SkyScanner, we believe that travel is not just a journey, 
+            but a transformative experience that opens doors to new cultures, 
+            perspectives, and unforgettable moments. As a leading travel 
+            planning website, we are here to make your travel dreams a 
+            reality.At SkyScanner, we believe in transparency and simplicity. 
+            We provide clear and concise information about each booking, including 
+            detailed descriptions, photos, user reviews, and ratings. We want you to 
+            feel confident in your choices and have peace of mind throughout your 
+            travel experience.So, whether you're looking to book a flight to a 
+            far-flung destination, find a cozy hotel for a weekend getaway, or 
+            rent a car to explore a new city, SkyScanner is here to be your trusted 
+            companion. Let us help you unlock the world and embark on incredible 
+            journeys that will broaden your horizons.</p>
 
-            <h1>Our Mission</h1>
-            <p>Montes nascetur ridiculus mus mauris vitae ultricies leo integer
-            Ut placerat orci nulla pellentesque dignissim enim sit. 
-            Sollicitudin nibh sit amet commodo nulla facilisi nullam. Turpis 
-            massa tincidunt dui ut. Vulputate sapien nec sagittis aliquam malesuada. 
-            Felis eget nunc lobortis mattis aliquam faucibus purus. Nibh nisl 
-            condimentum id venenatis a. Id volutpat lacus laoreet non curabitur 
-            gravida. Dolor sed viverra ipsum nunc. Bibendum ut tristique et 
-            egestas quis ipsum. Commodo quis imperdiet massa tincidunt nunc 
-            pulvinar sapien. </p>
+            <h1>OUR MISSION</h1>
+            <p>Our mission is simple: to empower travelers like you with the tools 
+            and information needed to plan, book, and enjoy seamless trips. Whether 
+            you're embarking on a solo adventure, a family vacation, or a business trip, 
+            we're committed to providing you with a comprehensive platform that simplifies 
+            the entire travel process.Our commitment to customer satisfaction is unwavering. 
+            Our dedicated support team is available around the clock to assist you with any 
+            inquiries or concerns you may have, ensuring that your travel plans go smoothly 
+            from start to finish.</p>
 
-            <h1>Our Vision</h1>
-            <p>Montes nascetur ridiculus mus mauris vitae ultricies leo integer
-            Ut placerat orci nulla pellentesque dignissim enim sit. 
-            Sollicitudin nibh sit amet commodo nulla facilisi nullam. Turpis 
-            massa tincidunt dui ut. Vulputate sapien nec sagittis aliquam malesuada. 
-            Felis eget nunc lobortis mattis aliquam faucibus purus. Nibh nisl 
-            condimentum id venenatis a. Id volutpat lacus laoreet non curabitur 
-            gravida. Dolor sed viverra ipsum nunc. Bibendum ut tristique et 
-            egestas quis ipsum. Commodo quis imperdiet massa tincidunt nunc 
-            pulvinar sapien. </p>
+            <h1>OUR VISION</h1>
+            <p>What sets us apart is our dedication to finding you the best travel deals. 
+            We understand that every traveler has unique preferences and budgets, and our 
+            powerful search engine scours through millions of flights, hotels, and car rental 
+            options to offer you the most competitive prices available. We're here to help you 
+            stretch your travel budget further, so you can focus on creating memories that will 
+            last a lifetime.But we're not just about finding you great deals; we're also passionate 
+            about inspiring you to explore new destinations. Our website features a wealth of travel 
+            guides, tips, and insider information, curated by our team of travel experts. From hidden 
+            gems and off-the-beaten-path adventures to iconic landmarks and must-see attractions, we 
+            strive to provide you with the knowledge you need to plan an extraordinary journey.</p>
+            
+            <h3>Start your travel adventure with SkyScanner today, and let the world become your playground.</h3>
        </div>
     );
 }
